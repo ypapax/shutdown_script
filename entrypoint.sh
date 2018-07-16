f9b54bbe2e41b3582ba2877d86ebf49eedb14926
Maxim Yefremov <maxYefr@gmail.com>
2018-07-16 14:57:49 +0300

    wait but still the same: can't wait cleaun end

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,16 @@ exec 2>&1
 date
 hostname
 
+
 cleanup(){
-	pkill -ef go_app
+	pkill -f go_app
+	while kill -0 $pID1; do
+	    sleep 1
+	done
+
+	while kill -0 $pID2; do
+	    sleep 1
+	done
 	curl -X POST -d "cleanup is called for $(hostname) $(date)" http://trap_exit.requestcatcher.com/test
 	echo cleanup is called
 	curl -X POST -d "cleanup is called log: $(cat $log)" http://trap_exit.requestcatcher.com/test
@@ -22,7 +30,9 @@ trap cleanup EXIT
 curl -X POST -d "container is started on $(hostname) $(date)" http://trap_exit.requestcatcher.com/test
 ls -la /root/scripts
 /root/go_app -v 4 -alsologtostderr -name app1 &
+pID1=$!
 /root/go_app -v 4 -alsologtostderr -name app2 &
+pID2=$!
 /root/scripts/second_script.sh -D
 
 sleep 10000
